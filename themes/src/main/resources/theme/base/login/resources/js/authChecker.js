a6fb9956c034224bf7e49e8d91d9bd3e80567823
t-tani <31282248+t-tani@users.noreply.github.com>
2024-11-08 13:38:56 +0900

    remove timer and add validation

--- themes/src/main/resources/theme/base/login/resources/js/authChecker.js
+++ themes/src/main/resources/theme/base/login/resources/js/authChecker.js
@@ -6,12 +6,12 @@ let timeout;
 
 // Remove the timeout when unloading to avoid execution of the
 // checkCookiesAndSetTimer when the page is already submitted
-addEventListener("beforeunload", () => {
-  if (timeout) {
-    clearTimeout(timeout);
-    timeout = undefined;
-  }
-});
+// addEventListener("beforeunload", () => {
+//   if (timeout) {
+//     clearTimeout(timeout);
+//     timeout = undefined;
+//   }
+// });
 
 export function checkCookiesAndSetTimer(loginRestartUrl) {
   if (initialSession) {
@@ -29,7 +29,9 @@ export function checkCookiesAndSetTimer(loginRestartUrl) {
     );
   } else {
     // Redirect to the login restart URL. This can typically automatically login user due the SSO
-    location.href = loginRestartUrl;
+    if (getCookieByName("KC_RESTART")) {
+      location.href = loginRestartUrl;
+    }
   }
 }
 
